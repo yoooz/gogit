225f5d01c612f4c085a8211ade1230b89ee67223
yoooz <oor.9@outlook.com>
2021-03-13 22:53:08 +0900

    aggregate commit per author

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
 
 func main() {
-	fmt.Printf("Hello world\n")
+	r, _ := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: "../test",
+	})
+
+	ref, _ := r.Head()
+	
+	cIter, _ := r.Log(&git.LogOptions{From: ref.Hash()})
+
+	authorMap := map[string][]string{}
+
+
+	cIter.ForEach(func(c *object.Commit) error {
+		authorMap[c.Author.Name] = append(authorMap[c.Author.Name], c.Message)
+		return nil
+	})
+
+	for key, value := range authorMap {
+		fmt.Println(key)
+		fmt.Println(value)
+		fmt.Println()
+	}
 }
